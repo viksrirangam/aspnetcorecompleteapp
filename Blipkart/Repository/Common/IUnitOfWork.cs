d1826bdb1f6cf7f8654f95b7c88b679420d1eb12
vikram srirangam <vikram.srirangam@outlook.com>
2016-07-31 09:31:25 +0530

    cleaning up code.

--- Blipkart/Repository/Common/IUnitOfWork.cs
+++ Blipkart/Repository/Common/IUnitOfWork.cs
@@ -1,18 +1,11 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Blipkart.Repository
 {
     public interface IUnitOfWork : IDisposable
     {
-
-        /// <summary>
-        /// Saves all pending changes
-        /// </summary>
-        /// <returns>The number of objects in an Added, Modified, or Deleted state</returns>
         int Commit();
     }
 }
